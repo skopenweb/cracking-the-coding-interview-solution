49b42f5aea2b992284efbc8b375a9d7fefb37035
Shubham Kumar <shubham.iitp@gmail.com>
2024-07-24 08:06:15 +0530

    chapter 1 ...more

--- src/main/kotlin/arrays/Arrays.kt
+++ src/main/kotlin/arrays/Arrays.kt
@@ -22,3 +22,70 @@ internal fun arrays_1_1_isUnique(s: String): Boolean {
     }
     return true
 }
+
+fun arrays_1_2_checkPermutation(a: String, b: String): Boolean {
+    if (a.length != b.length) return false
+
+    val freqArrayA = getFreq(a)
+    val frequencyB = getFreq(b)
+    return freqArrayA == frequencyB
+}
+
+private fun getFreq(s: String): Map<Char, Int> {
+    val freq = mutableMapOf<Char, Int>()
+    for (c in s) {
+        freq[c] = freq.getOrDefault(c, 0) + 1
+    }
+    return freq
+}
+
+fun arrays_1_3_URLify(s: String): String {
+    val urlifiedSpace = "%20"
+    val sb = StringBuilder()
+    s.forEach { ch ->
+        if (ch == ' ') {
+            sb.append(urlifiedSpace)
+        } else {
+            sb.append(ch)
+        }
+    }
+    return sb.toString()
+}
+
+fun arrays_1_3_URLify_using_arrray(s: String): String {
+    val urlifiedSpace = "%20"
+    val chArray = CharArray(s.length * 3)
+    var j = 0
+    for (c in s) {
+        if (c == ' ') {
+            chArray[j] = '%'
+            chArray[j + 1] = '2'
+            chArray[j + 2] = '0'
+            j += 3
+        } else {
+            chArray[j++] = c
+        }
+    }
+    return String(chArray, 0, j)
+}
+
+fun arrays_1_4_palindromic_permutation(s: String): Boolean {
+    val freqFlag = BooleanArray(27) {
+        false
+    }
+    for (c in s) {
+        if ('a'.code <= c.code && c.code <= 'z'.code) {
+            freqFlag[c.code - 'a'.code] = freqFlag[c.code - 'a'.code].not()
+        } else if ('A'.code <= c.code && c.code <= 'Z'.code) {
+            freqFlag[c.code - 'A'.code] = freqFlag[c.code - 'A'.code].not()
+        }
+    }
+    var count = 0
+    for (value in freqFlag) {
+        if (value) {
+            count++
+            if (count > 1) break
+        }
+    }
+    return count < 2
+}
--- src/test/kotlin/ArraysTest.kt
+++ src/test/kotlin/ArraysTest.kt
@@ -3,29 +3,24 @@ import kotlin.test.Test
 class ArraysTest {
 
     @Test
-    // string has all unique characters
     fun testUniqueTest() {
         val result = arrays_1_1_isUnique("abcdef")
         assert(result)
     }
 
     @Test
-    // string has all unique characters
     fun testUniqueTest2() {
         val result = arrays_1_1_isUnique("abcdefa")
         assert(result.not())
     }
 
     @Test
-    // string has all unique characters 3
     fun testUniqueTest3() {
         val result = arrays_1_1_isUnique("abadef")
         assert(result.not())
     }
 
-
     @Test
-    // string has all unique characters 3
     fun testUniqueTest4() {
         val result = arrays_1_1_isUnique("abba")
         assert(result.not())
